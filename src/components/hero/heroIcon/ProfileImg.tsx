97da5d2a897fa22afe888d46e6cdfe3ef45cf652
sekwan5 <sekwanoh1107@gmail.com>
2024-09-23 21:47:09 +0900

    add lazy load

--- src/components/hero/heroIcon/ProfileImg.tsx
+++ src/components/hero/heroIcon/ProfileImg.tsx
@@ -1,15 +1,11 @@
 import CoImage from "@/components/common/CoImages";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
+import LazyLoad from "react-lazyload";
 
 export default function ProfileImg(props: { id: string }) {
   const profileImage =
     "https://static.smilegatemegaport.com/event/live/epic7/guide/images/hero/c1159_s.png";
   const imgUrl = import.meta.env.VITE_ASSETS_URL;
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = `${imgUrl}/hero_images/${props.id}.png`;
-  }, [props.id, imgUrl]);
 
   return (
     <Fragment>
@@ -18,11 +14,13 @@ export default function ProfileImg(props: { id: string }) {
         src={"/images/img_profile_wrap.png"}
         alt="profile"
       />
-      <CoImage
-        className="profile-img"
-        src={`${imgUrl}/hero_images/${props.id}.png` || profileImage}
-        alt="profile"
-      />
+      <LazyLoad height={100} offset={100}>
+        <CoImage
+          className="profile-img"
+          src={`${imgUrl}/hero_images/${props.id}.png` || profileImage}
+          alt="profile"
+        />
+      </LazyLoad>
     </Fragment>
   );
 }
