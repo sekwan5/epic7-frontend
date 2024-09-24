b41b8f54b6a69dca56c507f064595833efb3f74d
sekwan5 <sekwanoh1107@gmail.com>
2024-09-25 07:10:37 +0900

    이미지인식 거의 완

--- src/components/gear/owner/ImageToText.tsx
+++ src/components/gear/owner/ImageToText.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef } from "react";
-import test from "@/modules/test.json";
 
 // Tesseract.js의 WorkerOptions 타입을 확장합니다.
 
@@ -12,7 +11,7 @@ const ImageToText = ({ setText }: ImageToTextProps) => {
   const [resultText, setResultText] = useState<string>("");
   const [processedImage, setProcessedImage] = useState<string | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
+  const visionApiKey = import.meta.env.VITE_GOOGLE_API_KEY;
   // 이미지 전처리 함수
   const processImage = (image: HTMLImageElement) => {
     const canvas = canvasRef.current;
@@ -29,15 +28,16 @@ const ImageToText = ({ setText }: ImageToTextProps) => {
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data;
 
-    const brightness = 35;
-    const contrast = 1.7;
+    const brightnessThreshold = 100; // 이 값 이하의 밝기는 제거
 
     for (let i = 0; i < data.length; i += 4) {
-      let avg = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
-      avg = (avg - 128) * contrast + 128;
-      avg += brightness;
-      avg = Math.max(0, Math.min(255, avg));
-      data[i] = data[i + 1] = data[i + 2] = avg;
+      // RGB 값을 평균하여 밝기 계산
+      const avg = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
+
+      // 밝기가 threshold 이하인 경우 픽셀을 완전히 투명하게 설정
+      if (avg < brightnessThreshold) {
+        data[i + 3] = 0; // Alpha 값을 0으로 설정하여 픽셀을 투명하게 만듦
+      }
     }
 
     ctx.putImageData(imageData, 0, 0);
@@ -64,52 +64,154 @@ const ImageToText = ({ setText }: ImageToTextProps) => {
     }
   };
 
+  type ParsedData = {
+    key: string;
+    value: string;
+  };
+
+  type Result = {
+    parsedData: ParsedData[];
+    set: string | null;
+  };
+
+  function parseEquipmentData(dataList: string[]): Result {
+    const parsedData: ParsedData[] = [];
+    let setInfo: string | null = null;
+    const validKeys = [
+      "생명력",
+      "방어력",
+      "속도",
+      "치명확률",
+      "치명피해",
+      "공격력",
+      "효과저항",
+      "효과적중",
+      "속도",
+    ];
+
+    for (let i = 0; i < dataList.length; i++) {
+      const currentKey = dataList[i].replace(/[^가-힣a-zA-Z0-9%]/g, "").trim();
+      // 세트 정보 추출
+
+      // 유효한 키인지 확인
+      if (validKeys.includes(currentKey)) {
+        let j = i + 1;
+        let value = "";
+
+        while (j < dataList.length) {
+          const potentialValue = dataList[j].replace(/[^0-9.%]/g, "").trim();
+          if (validKeys.includes(dataList[i - 1])) {
+            value = dataList[j + 1];
+            break;
+          }
+
+          if (
+            potentialValue.endsWith("%") ||
+            !isNaN(parseFloat(potentialValue))
+          ) {
+            if (currentKey === "속도" && potentialValue.endsWith("%")) {
+              const nextValue = dataList[j + 1]
+                ?.replace(/[^0-9.%]/g, "")
+                .trim();
+              value = nextValue || potentialValue;
+              break;
+            }
+            if (
+              (currentKey === "공격력" ||
+                currentKey === "생명력" ||
+                currentKey === "방어력") &&
+              (potentialValue === "6" || potentialValue === "15")
+            ) {
+              // 다음 값을 확인
+              const nextValue = dataList[j + 1]
+                ?.replace(/[^0-9.%]/g, "")
+                .trim();
+              if (nextValue === "6" || nextValue === "15") {
+                // 두 번째 값도 6 또는 15이면 다음으로 넘어감
+                j += 2;
+                continue;
+              } else {
+                // 두 번째 값이 유효하면 그 값을 사용
+                value = nextValue || potentialValue;
+                break;
+              }
+            } else {
+              // 일반적인 경우, 첫 번째 유효한 값을 사용
+              value = potentialValue;
+              break;
+            }
+          }
+          j++;
+        }
+
+        if (value) {
+          parsedData.push({ key: currentKey, value });
+          // i = j; // i를 찾은 값의 위치로 업데이트
+        }
+      }
+
+      if (currentKey.includes("세트")) {
+        const setParts = currentKey.match(/([가-힣]+)의세트/);
+        if (setParts && setParts[1]) {
+          setInfo = setParts[1];
+        }
+        continue;
+      }
+    }
+
+    return {
+      parsedData,
+      set: setInfo,
+    };
+  }
   const handleExtractText = async () => {
     if (!canvasRef.current) return;
 
-    // const canvas = canvasRef.current;
-    // const imageDataUrl = canvas.toDataURL("image/png");
-    // const base64Image = imageDataUrl.replace(
-    //   /^data:image\/(png|jpeg|jpg);base64,/,
-    //   "",
-    // );
+    const canvas = canvasRef.current;
+    const imageDataUrl = canvas.toDataURL("image/png");
+    const base64Image = imageDataUrl.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      "",
+    );
 
-    // Tesseract.js를 사용하여 선택된 영역의 텍스트만 추출
-    // const apiKey = "AIzaSyDapu8iTty2V_6E7Kk70KohQE0j2vcb0M8"; // 여기에 Google Vision API 키를 입력하세요
-    // const visionApiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+    const apiKey = visionApiKey;
+    const visionApiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
 
-    // const requestPayload = {
-    //   requests: [
-    //     {
-    //       image: {
-    //         content: base64Image,
-    //       },
-    //       features: [
-    //         {
-    //           type: "TEXT_DETECTION",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // };
+    const requestPayload = {
+      requests: [
+        {
+          image: {
+            content: base64Image,
+          },
+          features: [
+            {
+              type: "TEXT_DETECTION",
+            },
+          ],
+        },
+      ],
+    };
 
     try {
-      // const response = await fetch(visionApiUrl, {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(requestPayload),
-      // });
-      const response = test;
+      const response = await fetch(visionApiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestPayload),
+      });
+      // const response = test;
 
-      // const result = await response.json();
-      const result = response;
-      const extractedText = result[0].fullTextAnnotation
-        ? result[0].fullTextAnnotation.text
+      const result = await response.json();
+      // const result = response;
+      console.log("result", result);
+      const extractedText = result.responses[0].fullTextAnnotation
+        ? result.responses[0].fullTextAnnotation.text
         : "No text found";
       const list = extractedText.split("\n");
       console.log("list", list);
+      const data = parseEquipmentData(list);
+      console.log("data", data);
       // const extractedText = result.responses[0].fullTextAnnotation
       //   ? result.responses[0].fullTextAnnotation.text
       //   : "No text found";
@@ -120,6 +222,7 @@ const ImageToText = ({ setText }: ImageToTextProps) => {
       setText("텍스트 추출 실패");
     }
   };
+
   return (
     <div className="img-to-text">
       <div className="d-flex">
